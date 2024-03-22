db325043395f95f12fb9f65139b08af256e8f805
evasousa23 <sousa.eva99@gmail.com>
2024-03-22 18:11:44 +0000

    create date

---
+++ src/FormatDateForecast.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FormatDateForecast(props) {
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[props.date.getDay()];
+
+
+    return <div>{day}</div>;
+}
